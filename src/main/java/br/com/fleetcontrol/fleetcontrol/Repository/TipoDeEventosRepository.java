761a7425a7d7045fe362b0c6fb868cea77eaebeb
MatheuselMarge <elmargematheuspaes@gmail.com>
2023-04-19 19:55:17 -0300

    atualização de @Entity

--- src/main/java/br/com/fleetcontrol/fleetcontrol/Repository/TipoDeEventosRepository.java
+++ src/main/java/br/com/fleetcontrol/fleetcontrol/Repository/TipoDeEventosRepository.java
@@ -2,6 +2,6 @@ package br.com.fleetcontrol.fleetcontrol.Repository;
 
 import br.com.fleetcontrol.fleetcontrol.Entity.TipoDeEventos;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface TipoDeEventosRepository extends JpaRepository<TipoDeEventos,Long> {
 }
--- src/main/java/br/com/fleetcontrol/fleetcontrol/Repository/UsuarioRepository.java
+++ src/main/java/br/com/fleetcontrol/fleetcontrol/Repository/UsuarioRepository.java
@@ -2,6 +2,6 @@ package br.com.fleetcontrol.fleetcontrol.Repository;
 
 import br.com.fleetcontrol.fleetcontrol.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
 }
