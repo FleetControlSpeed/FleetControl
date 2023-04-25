2d17f0ccf3805ea4e3994b57c2fc519cd841363d
Cris <cristovaom22@gmail.com>
2023-04-25 12:54:21 -0300

    Retirei a Tipo de Eventos , Arrumei algumas nomenclaturas e adicionei algumas Anotações.

--- src/main/java/br/com/fleetcontrol/fleetcontrol/Entity/Modelo.java
+++ src/main/java/br/com/fleetcontrol/fleetcontrol/Entity/Modelo.java
@@ -1,20 +1,30 @@
 package br.com.fleetcontrol.fleetcontrol.Entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+@NoArgsConstructor
 @Entity
 @Table(name = "tb_Modelo", schema = "public")
 public class Modelo extends AbstractEntity{
     @Getter
     @Setter
-    @Column(name = "nome",nullable = false)
+    @NotNull
+    @NotEmpty
+    @Size(min = 3, max = 20)
+    @Column(name = "nome",nullable = false,length = 20)
     private String nome;
 
     @Getter
     @Setter
+    @NotNull
+    @NotEmpty
     @Enumerated(EnumType.STRING)
+    @Column(name = "marca",nullable = false)
     private Marca marca;
-
 
 }
