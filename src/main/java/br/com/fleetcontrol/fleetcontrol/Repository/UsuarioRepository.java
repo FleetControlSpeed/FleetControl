b3c76f215af6e4efa2bda02b29bf5dd61865f5b4
Cris <cristovaom22@gmail.com>
2023-05-06 14:26:23 -0300

    Service Inicial e 2 Querys no Repository de Usuario

--- src/main/java/br/com/fleetcontrol/fleetcontrol/Repository/UsuarioRepository.java
+++ src/main/java/br/com/fleetcontrol/fleetcontrol/Repository/UsuarioRepository.java
@@ -2,7 +2,13 @@ package br.com.fleetcontrol.fleetcontrol.Repository;
 
 import br.com.fleetcontrol.fleetcontrol.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /*
     @Author: Cristovão Martins
     Date: 06/05/2023
@@ -10,4 +16,11 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+
+    @Modifying
+    @Query("UPDATE Usuario usuario SET usuario.ativo = false WHERE usuario.id = :idUsuario")
+    public void desativar(@Param("idUsuario") Long id);
+
+    @Query("SELECT usuario FROM Usuario usuario WHERE usuario.ativo = true")
+    public List<Usuario> UsuariosAtivos();
 }
---
+++ src/main/java/br/com/fleetcontrol/fleetcontrol/Service/UsuarioService.java
@@ -0,0 +1,53 @@
+package br.com.fleetcontrol.fleetcontrol.Service;
+
+import br.com.fleetcontrol.fleetcontrol.Entity.Usuario;
+import br.com.fleetcontrol.fleetcontrol.Repository.UsuarioRepository;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario cadastrar(Usuario usuario) {
+        if(usuario.getUsuario().trim().isEmpty()){
+            throw  new RuntimeException("Erro usuario Nulo!");
+        }else{
+            return this.usuarioRepository.save(usuario);
+        }
+    }
+    public List<Usuario> listaCompleta() {
+        return this.usuarioRepository.findAll();
+    }
+
+    public Usuario findById(Long id) {
+        return this.usuarioRepository.findById(id).orElse(new Usuario());
+    }
+    @Transactional
+    public void atualizar(Long id, Usuario usuario) {
+        if(id == usuario.getId()) {
+            this.usuarioRepository.save(usuario);
+        }
+    }
+
+    @Transactional
+    public void desativar(Long id){
+        var usuario = this.usuarioRepository.findById(id);
+        if (id == usuario.get().getId()) {
+            this.usuarioRepository.desativar(id);
+        }
+    }
+
+    public List<Usuario> listaVeiculosAtivos(){
+
+        return this.usuarioRepository.UsuariosAtivos();
+    }
+}
