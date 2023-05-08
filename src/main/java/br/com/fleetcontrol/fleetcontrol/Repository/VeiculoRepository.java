c2c9c7c7a8dedfdd4c714cba4ede23844ce92b3d
Ademar Ramalho <ademar.neto@descomplica.com.br>
2023-05-08 20:07:12 -0300

    Criando Service e Repository Veiculo

--- src/main/java/br/com/fleetcontrol/fleetcontrol/Repository/VeiculoRepository.java
+++ src/main/java/br/com/fleetcontrol/fleetcontrol/Repository/VeiculoRepository.java
@@ -1,9 +1,98 @@
 package br.com.fleetcontrol.fleetcontrol.Repository;
 
+import br.com.fleetcontrol.fleetcontrol.Entity.Eventos;
 import br.com.fleetcontrol.fleetcontrol.Entity.Veiculo;
+import br.com.fleetcontrol.fleetcontrol.Service.EventosService;
+import br.com.fleetcontrol.fleetcontrol.Service.VeiculoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
 
-@Repository
- public interface VeiculoRepository extends JpaRepository<Veiculo,Long> {
+import java.util.Collection;
+
+@Controller
+@ResponseBody
+@RequestMapping(value = "/api/eventos")
+public class VeiculoController() {
+    @Autowired
+    private VeiculoService service;
+
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable("id") final Long id) {
+        try {
+            final Veiculo veiculo = service.buscarPorId(id);
+            return ResponseEntity.ok(veiculo);
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error " + e.getMessage());
+        }
+    }
+
+    @GetMapping(value = "/listar")
+    public ResponseEntity<?> listar() {
+        try {
+            return ResponseEntity.ok(service.listar());
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error " + e.getMessage());
+        }
+    }
+
+    @GetMapping(value = "/listarPorAtivo")
+    public ResponseEntity<?> listarPorAtivo() {
+        try {
+            return ResponseEntity.ok(service.listarPorAtivo());
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error " + e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> cadastrar(@RequestParam("id") final Veiculo veiculo) {
+        try {
+            this.service.salvar(veiculo);
+            return ResponseEntity.ok("Evento cadastrado com sucesso!");
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error" + e.getMessage());
+        }
+    }
+
+    @PutMapping(value = "/editar")
+    public ResponseEntity<?> editar(@RequestParam("id") final Long id, @RequestBody final Veiculo veiculoNovo) {
+        try {
+            service.editar(id, veiculoNovo);
+            return ResponseEntity.ok("Evento atualizado com sucesso!");
+
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body("Error" + e.getMessage());
+        }
+    }
+
+    @PutMapping(value = "/desativar")
+    public ResponseEntity<?> desativar(@RequestParam("id") final Long id) {
+        try {
+            service.desativar(id);
+            return ResponseEntity.ok("Veiculo desativado com sucesso!");
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error" + e.getMessage());
+        }
+    }
+
+    @PutMapping(value = "/ativar")
+    public ResponseEntity<?> ativar(@RequestParam("id") final Long id) {
+        try {
+            service.ativar(id);
+            return ResponseEntity.ok("Veiculo ativado com sucesso!");
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error " + e.getMessage());
+        }
+    }
 }
--- src/main/java/br/com/fleetcontrol/fleetcontrol/Service/VeiculoService.java
+++ src/main/java/br/com/fleetcontrol/fleetcontrol/Service/VeiculoService.java
@@ -1,4 +1,82 @@
 package br.com.fleetcontrol.fleetcontrol.Service;
 
+import br.com.fleetcontrol.fleetcontrol.Entity.Eventos;
+import br.com.fleetcontrol.fleetcontrol.Entity.Veiculo;
+import br.com.fleetcontrol.fleetcontrol.Repository.VeiculoController;
+import br.com.fleetcontrol.fleetcontrol.Repository.VeiculoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class VeiculoService {
+    @Autowired
+    private VeiculoController repository;
+    public Veiculo buscarPorId(Long id) {
+
+        if (id == 0) {
+            throw new RuntimeException(", por favor, informe um valor valido!");
+
+        } else if (repository.findById(id).isEmpty()) {
+            throw new RuntimeException(", não foi possivel localizar o condutor informado!");
+
+        } else {
+            return repository.findById(id).orElse(null);
+        }
+    }
+
+    public List<Veiculo> listar() {
+        if (repository.findAll().isEmpty()) {
+            throw new RuntimeException(", banco de dados não possui eventos cadastrados!");
+
+        } else {
+            return repository.findAll();
+        }
+    }
+
+    public List<Veiculo> listarPorAtivo() {
+        if (repository.buscarPorAtivo().isEmpty()) {
+            throw new RuntimeException(", banco de dados não possui eventos ativos!");
+
+        } else {
+            return repository.buscarPorAtivo();
+        }
+    }
+
+    public Veiculo salvar(Veiculo veiculo) {
+        return this.repository.save(veiculo);
+    }
+
+    public void editar(Long id, Veiculo veiculoNovo){
+        final Veiculo veiculoBanco= this.buscarPorId(id);
+
+        if(veiculoBanco == null || !veiculoBanco.getId().equals(veiculoNovo.getId())){
+            throw new RuntimeException(", não foi possivel identificar o evento informado!");
+
+        } else {
+            this.salvar(veiculoNovo);
+        }
+    }
+
+    public void desativar(Long id) {
+        Veiculo veiculo = buscarPorId(id);
+
+        if (!veiculo.isAtivo()) {
+            throw new RuntimeException(", evento informado já esta desativado!");
+
+        } else {
+            repository.desativar(id);
+        }
+    }
+
+    public void ativar(Long id) {
+        Veiculo veiculo = buscarPorId(id);
+
+        if (veiculo.isAtivo()) {
+            throw new RuntimeException(", evento informado já esta ativado!");
+        } else {
+            repository.ativar(id);
+        }
+    }
 }
