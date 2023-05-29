c022e79aacc6ef00027f1d0b8c0c42240f858170
cris <cristovaom22@gmail.com>
2023-05-29 02:30:58 -0300

    hotfix(DatabaseException): Arrumando erro do conteudo das classes nao aparecer

--- src/main/java/br/com/fleetcontrol/fleetcontrol/service/exceptions/DatabaseException.java
+++ src/main/java/br/com/fleetcontrol/fleetcontrol/service/exceptions/DatabaseException.java
@@ -1,2 +1,6 @@
-package br.com.fleetcontrol.fleetcontrol.service.exceptions;public class DatabaseException {
+package br.com.fleetcontrol.fleetcontrol.service.exceptions;
+public class DatabaseException extends RuntimeException{
+    public DatabaseException(String msg){
+        super(msg);
+    }
 }
