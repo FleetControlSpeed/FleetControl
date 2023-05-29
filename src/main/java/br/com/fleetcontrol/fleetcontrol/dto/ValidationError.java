f8f9df612f1a428f5f5a745a1b2e82c3acb0ad3d
cris <cristovaom22@gmail.com>
2023-05-29 02:29:33 -0300

    hotfix(ValidationError): Arrumando erro do conteudo das classes nao aparecer

--- src/main/java/br/com/fleetcontrol/fleetcontrol/dto/ValidationError.java
+++ src/main/java/br/com/fleetcontrol/fleetcontrol/dto/ValidationError.java
@@ -1,3 +1,21 @@
 package br.com.fleetcontrol.fleetcontrol.dto;
-public class ValidationError {
+
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends CustomError{
+    @Getter
+    public List<FieldMessage> errors = new ArrayList<>();
+
+
+    public ValidationError(Instant timestamp, Integer status, String error, String path) {
+        super(timestamp, status, error, path);
+    }
+
+    public void addError(String fieldName,String message){
+        errors.add(new FieldMessage(fieldName,message));
+    }
 }
