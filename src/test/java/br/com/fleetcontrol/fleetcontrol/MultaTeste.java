075627279a8be5c90fe72834fbf5bbf3aab47903
dogogh <quadros1919@gmail.com>
2023-10-02 10:23:26 -0300

    Multa é para funcionar

--- src/test/java/br/com/fleetcontrol/fleetcontrol/MultaTeste.java
+++ src/test/java/br/com/fleetcontrol/fleetcontrol/MultaTeste.java
@@ -2,20 +2,23 @@ package br.com.fleetcontrol.fleetcontrol;
 
 import br.com.fleetcontrol.fleetcontrol.controller.MultaController;
 import br.com.fleetcontrol.fleetcontrol.entity.*;
-import org.springframework.data.domain.Page;
 
+
+import br.com.fleetcontrol.fleetcontrol.repository.MultaRepository;
 import br.com.fleetcontrol.fleetcontrol.service.MultaService;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import org.springframework.data.domain.Pageable;
+
+import org.springframework.test.web.servlet.MockMvc;
 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.*;
 
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
@@ -29,11 +32,16 @@ import java.util.List;
 
 public class MultaTeste {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @InjectMocks
     private MultaController controller;
 
     @Mock
     private MultaService service;
+    @Mock
+    private MultaRepository repository;
 
     @BeforeEach
     void setUp() {
@@ -41,23 +49,11 @@ public class MultaTeste {
     }
 
     @Test
-    void testBuscarPorId() {
-        Long idMulta = 1L;
-        Multa multa = new Multa();
-        when(service.buscarPorId(idMulta)).thenReturn(multa);
-
-        ResponseEntity<?> response = controller.buscarPorId(idMulta);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        Multa multaNaResposta = (Multa) response.getBody();
-        assertNotNull(multaNaResposta);
-    }
-
-    @Test
-    void testListar() {
+    void testListarSucesso() {
         List<Multa> multasList = new ArrayList<>();
         multasList.add(new Multa());
-        when(service.listaCompleta(any())).thenReturn(Page.empty());
-        ResponseEntity<?> response = controller.listar(Pageable.unpaged());
+        when(service.listaCompleta()).thenReturn(multasList);
+        ResponseEntity<?> response = controller.listar();
         assertEquals(HttpStatus.OK, response.getStatusCode());
 
         List<Multa> multasNaResposta = (List<Multa>) response.getBody();
@@ -66,53 +62,54 @@ public class MultaTeste {
     }
 
     @Test
-    void testCadastrar() {
-        Multa multa = new Multa();
-        when(service.salvar(multa)).thenReturn(multa);
-
-        ResponseEntity<?> response = controller.cadastrar(multa);
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        Multa multaCriada = (Multa) response.getBody();
-        assertNotNull(multaCriada);
-    }
-
-
-    @Test
-    void testAtualizar() {
-        Long idMulta = 1L;
-        Multa multa = new Multa();
-        when(service.buscarPorId(idMulta)).thenReturn(multa);
-        doNothing().when(service).atualizar(idMulta, multa);
-
-        ResponseEntity<?> response = controller.atualizar(idMulta, multa);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("Multa alterada com sucesso!", response.getBody());
-        verify(service, times(1)).atualizar(idMulta, multa);
+    void testListarErro() {
+        when(service.listaCompleta()).thenThrow(new RuntimeException("Multa não encontrada!"));
+        ResponseEntity<?> response = controller.listar();
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        String mensagemErro = (String) response.getBody();
+        assertNotNull(mensagemErro);
+        assertTrue(mensagemErro.contains("Multa não encontrada!"));
     }
 
     @Test
-    void testDesativar() {
+    void testBuscarPorIdSucesso() {
         Long idMulta = 1L;
         Multa multa = new Multa();
         when(service.buscarPorId(idMulta)).thenReturn(multa);
-        doNothing().when(service).desativar(idMulta);
 
-        ResponseEntity<?> response = controller.desativar(idMulta);
+        ResponseEntity<?> response = controller.buscarPorId(idMulta);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("Multa desativada com sucesso!", response.getBody());
-        verify(service, times(1)).desativar(idMulta);
+        MultaDTO multaDTO = (MultaDTO) response.getBody();
+        assertNotNull(multaDTO);
     }
 
     @Test
-    void testAtivar() {
-        Long idMulta = 1L;
-        Multa multa = new Multa();
-        when(service.buscarPorId(idMulta)).thenReturn(multa);
-        doNothing().when(service).ativar(idMulta);
+    void testBuscarPorIdErro() {
+        Long idMulta = 0L;
+        when(service.buscarPorId(idMulta)).thenThrow(new RuntimeException("Multa não encontrada!"));
+        ResponseEntity<?> response = controller.buscarPorId(idMulta);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
 
-        ResponseEntity<?> response = controller.ativar(idMulta);
+        String mensagemErro = (String) response.getBody();
+        assertNotNull(mensagemErro);
+        assertTrue(mensagemErro.contains("Multa não encontrada!"));
+    }
+    @Test
+    void testCadastrarSucesso() {
+        MultaDTO multaDTO = new MultaDTO();
+        doNothing().when(service).salvar(multaDTO);
+        ResponseEntity<?> response = controller.cadastrar(multaDTO);
+
+        verify(service, times(1)).salvar(multaDTO);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("Multa ativada com sucesso!", response.getBody());
-        verify(service, times(1)).ativar(idMulta);
+    }
+
+
+    private String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
